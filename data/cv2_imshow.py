c75abc70f0634502a6c3518bfcdb14e43e4716ce
Noushad Khan <noushadkhan1994@gmail.com>
2019-03-20 22:45:00 +0530

    Update cv2_imshow.py

--- data/cv2_imshow.py
+++ data/cv2_imshow.py
@@ -8,7 +8,7 @@ def cv2_imshow(a):
   # cv2 stores colors as BGR; convert to RGB
   if a.ndim == 3:
     if a.shape[2] == 4:
-      a = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+      a = cv2.cvtColor(a, cv2.COLOR_BGRA2RGBA)
     else:
-      a = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
   display(PIL.Image.fromarray(a))
