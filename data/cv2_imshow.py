79b1e3935908310ef4e919dd571a89b3577e87ad
Noushad Khan <noushadkhan1994@gmail.com>
2019-03-20 22:45:51 +0530

    Update cv2_imshow.py

--- data/cv2_imshow.py
+++ data/cv2_imshow.py
@@ -1,14 +1,14 @@
-def cv2_imshow(a):
+def cv2_imshow(img):
   import cv2
   import numpy as np
   import PIL
   from IPython.display import display
   """A replacement for cv2.imshow() for use in Jupyter notebooks."""
-  a = a.clip(0, 255).astype('uint8')
+  img = img.clip(0, 255).astype('uint8')
   # cv2 stores colors as BGR; convert to RGB
-  if a.ndim == 3:
-    if a.shape[2] == 4:
-      a = cv2.cvtColor(a, cv2.COLOR_BGRA2RGBA)
+  if img.ndim == 3:
+    if img.shape[2] == 4:
+      img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
     else:
-      a = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
-  display(PIL.Image.fromarray(a))
+      img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  display(PIL.Image.fromarray(img))
